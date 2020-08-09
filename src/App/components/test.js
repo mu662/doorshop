361ca5aa86dddd93846137b22f6c7a1d33b38e1d
unknown <mukeshrawat5666@gmail.com>
2020-08-09 16:33:42 +0530

    test

---
+++ src/App/components/test.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class SamplePage extends Component {
+    render() {
+        return (
+           <p>Test Page</p>
+        );
+    }
+}
+
+export default SamplePage;
